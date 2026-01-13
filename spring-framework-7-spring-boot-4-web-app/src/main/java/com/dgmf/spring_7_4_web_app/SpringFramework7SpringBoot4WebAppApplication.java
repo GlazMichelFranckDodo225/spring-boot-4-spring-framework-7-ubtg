f873769549527efdf155d589f56fbf42c8b854ab
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-01-13 07:57:08 +0100

    Project Initialization

---
+++ spring-framework-7-spring-boot-4-web-app/src/main/java/com/dgmf/spring_7_4_web_app/SpringFramework7SpringBoot4WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.spring_7_4_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework7SpringBoot4WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramework7SpringBoot4WebAppApplication.class, args);
+	}
+
+}
---
+++ spring-framework-7-spring-boot-4-web-app/src/test/java/com/dgmf/spring_7_4_web_app/SpringFramework7SpringBoot4WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.spring_7_4_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework7SpringBoot4WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
